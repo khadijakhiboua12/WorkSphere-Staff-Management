0abe9cefc8da9c1aba7bc0323fe24af1416b0a97
khadijakhiboua12 <khadijakhiboua12@gmail.com>
2025-11-20 12:30:03 +0100

    ajoute le scrolle pour l'affichage des caetes en partie zone d'employer

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //Tableau  contient les donnes 
- let tabEmployer =  JSON.parse(localStorage.getItem("employer")) || [
+let tabEmployer = JSON.parse(localStorage.getItem("employer")) || [
     {
         id: 1,
         Nom: "hello",
@@ -7,10 +7,10 @@
         Url: "Image_profile1.jpg",
         email: "khadija@gmail.com",
         phone: "061234567",
-        is_worked:false,
-        zone_work:null ,
+        is_worked: false,
+        zone_work: null,
         Experience: [
-            { Entreprise: "hhhd", Post: "Receptionniste", dateDeub: "12-04-2002" ,dateFin:"12-07-2002"}
+            { Entreprise: "hhhd", Post: "Receptionniste", dateDeub: "12-04-2002", dateFin: "12-07-2002" }
         ]
     },
     {
@@ -20,33 +20,33 @@
         Url: "Image_profile2.jpg",
         email: "youssef@gmail.com",
         phone: "0611223344",
-         is_worked:false,
-        zone_work:null,
+        is_worked: false,
+        zone_work: null,
         Experience: [
-            { Entreprise: "ABC Corp", Post: "Manager", dateDeub: "12-04-2002" ,dateFin:"12-07-2002" }
+            { Entreprise: "ABC Corp", Post: "Manager", dateDeub: "12-04-2002", dateFin: "12-07-2002" }
         ]
     }
 ];
-let container=document.getElementById("cards-container");
-let container_reseption_ =document.getElementById("container_Réception")
+let container = document.getElementById("cards-container");
+let container_reseption_ = document.getElementById("container_Réception")
 //fonction de setItem
-function setEmployer(tabEmployer){
-   localStorage.setItem("employer",JSON.stringify(tabEmployer));
+function setEmployer(tabEmployer) {
+    localStorage.setItem("employer", JSON.stringify(tabEmployer));
 }
 setEmployer(tabEmployer);
 //fonction de getItem
-function   getEmployer(){
-     return  JSON.parse(localStorage.getItem("employer") || []);
+function getEmployer() {
+    return JSON.parse(localStorage.getItem("employer") || []);
 }
 
 //Affichage la  cartes  de  la page
-let data=getEmployer();
-function AfficherCarte(data){
-    container.innerHTML="";
-   for(let i=0;i<data.length;i++){
-        const employe=data[i];
-        if(employe.is_worked==false){
-      container.innerHTML+=`
+let data = getEmployer();
+function AfficherCarte(data) {
+    container.innerHTML = "";
+    for (let i = 0; i < data.length; i++) {
+        const employe = data[i];
+        if (employe.is_worked == false) {
+            container.innerHTML += `
     <div class="shadow-lg rounded-xl p-4 m-2 flex items-center gap-4 "> 
     <img src="${employe.Url}" class="rounded-full w-20 h-20">
    
@@ -58,9 +58,9 @@ function AfficherCarte(data){
     </div>
     
      `;
-     }
-   }
-     
+        }
+    }
+
 }
 AfficherCarte(tabEmployer);
 //Fonction pour add plusieur expirience
@@ -94,29 +94,29 @@ function addExperience() {
     container.prepend(experienceDiv);
 }
 //function pour ajouter les employer
-function AddWorker(modalId){
-    const modal=document.getElementById(modalId);
+function AddWorker(modalId) {
+    const modal = document.getElementById(modalId);
     modal.classList.toggle("hidden");
 }
-document.getElementById("btn").addEventListener("click",()=>AddWorker("modal"));
-document.getElementById("btncancel").addEventListener("click",()=>AddWorker("modal"));
+document.getElementById("btn").addEventListener("click", () => AddWorker("modal"));
+document.getElementById("btncancel").addEventListener("click", () => AddWorker("modal"));
 
 //preview d'un image 
 function previewPhoto() {
     const photoInput = document.getElementById("photoInput");
     const photoPreview = document.getElementById("photoPreview");
     const urlError = document.getElementById("urlError");
-    photoInput.addEventListener("input", function() {
+    photoInput.addEventListener("input", function () {
         const url = photoInput.value.trim();
         const imageRegex = /^[\w,\s-]+\.(jpg|jpeg|png|gif)$/i;
 
-        if(url === "") {
+        if (url === "") {
             photoPreview.src = "";
             photoPreview.classList.add("hidden");
             urlError.textContent = "";
             return;
         }
-        if(imageRegex.test(url)) {
+        if (imageRegex.test(url)) {
             photoPreview.src = url;
             photoPreview.classList.remove("hidden");
             urlError.textContent = "";
@@ -140,55 +140,55 @@ function validateEmployeeForm() {
     const nomError = document.getElementById("nomError");
     const emailError = document.getElementById("emailError");
     const phoneError = document.getElementById("phoneError");
-    const urlError = document.getElementById("urlError");    
+    const urlError = document.getElementById("urlError");
     const nameRegex = /^[A-Za-z]{2,20}$/;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const phoneRegex = /^[0-9]{8,15}$/;
-    const urlRegex =  /^[\w,\s-]+\.(jpg|jpeg|png|gif)$/i;
+    const urlRegex = /^[\w,\s-]+\.(jpg|jpeg|png|gif)$/i;
     nomError.textContent = "";
     emailError.textContent = "";
     phoneError.textContent = "";
     urlError.textContent = "";
-    if(!nameRegex.test(nom)) {
+    if (!nameRegex.test(nom)) {
         nomError.textContent = "Nom invalide";
         isValid = false;
     }
-    if(!emailRegex.test(email)) {
+    if (!emailRegex.test(email)) {
         emailError.textContent = "Email invalide !";
         isValid = false;
     }
-    if(!phoneRegex.test(phone)) {
+    if (!phoneRegex.test(phone)) {
         phoneError.textContent = "Numéro invalide !";
         isValid = false;
     }
-    if(!urlRegex.test(url)) {
+    if (!urlRegex.test(url)) {
         urlError.textContent = "URL invalide !";
         isValid = false;
     }
     const experienceDivs = document.querySelectorAll("#experience-container > div");
-    for(let i=0; i<experienceDivs.length; i++){
+    for (let i = 0; i < experienceDivs.length; i++) {
         const div = experienceDivs[i];
         const entreprise = div.querySelector("input[name='entreprise']");
         const post = div.querySelector("input[name='post']");
         const dateDebut = div.querySelector("input[name='dateDebut']");
         const dateFin = div.querySelector("input[name='dateFin']");
-        if(!div.querySelector(".expError")){
+        if (!div.querySelector(".expError")) {
             const span = document.createElement("span");
             span.className = "text-red-600 text-sm expError";
             div.appendChild(span);
         }
         const expError = div.querySelector(".expError");
         expError.textContent = "";
-        if(entreprise.value.trim() === "" || post.value.trim() === "" || dateDebut.value === "" || dateFin.value === ""){
+        if (entreprise.value.trim() === "" || post.value.trim() === "" || dateDebut.value === "" || dateFin.value === "") {
             expError.textContent = `Tous les champs de l'expérience  sont obligatoires`;
             isValid = false;
         }
     }
     return isValid;
 }
-document.getElementById("btnsave").addEventListener("click", function(e){
+document.getElementById("btnsave").addEventListener("click", function (e) {
     e.preventDefault();
-    if(validateEmployeeForm()){
+    if (validateEmployeeForm()) {
         saveEmployee();
         alert("Employe sauvegarde avec succe ");
     }
@@ -220,8 +220,8 @@ function saveEmployee() {
         email: email,
         phone: phone,
         Experience: experiences,
-        is_worked:false,
-        zone_work:null 
+        is_worked: false,
+        zone_work: null
 
     };
     tabEmployer.push(newEmployee);
@@ -231,131 +231,128 @@ function saveEmployee() {
     document.getElementById("modal").classList.add("hidden");
     AfficherCarte(tabEmployer);
 }
-//L parie de zone
-let _room1 =document.getElementById("room1")
-let _room2 =document.getElementById("room2")
-let _room3 =document.getElementById("room3")
-let _room4 =document.getElementById("room4")
-let _room5 =document.getElementById("room5")
-let _room6 =document.getElementById("room6")
+//La par de zone
+let _room1 = document.getElementById("room1")
+let _room2 = document.getElementById("room2")
+let _room3 = document.getElementById("room3")
+let _room4 = document.getElementById("room4")
+let _room5 = document.getElementById("room5")
+let _room6 = document.getElementById("room6")
 
 
 
 
 
+function afficherEmployesZone(room_name, container_) {
 
+    const roles_zone = {
+    "Reception": ["Manager", "Réceptionniste", "Nettoyage"],
+    "Salle des serveurs": ["Manager", "Technicien IT", "Nettoyage"],
+    "Salle de sécurité": ["Manager", "Agent de sécurité", "Nettoyage"],
+    "Salle du personnel": ["Manager", "Réceptionniste", "Technicien IT", "Agent de sécurité", "Nettoyage", "Autres"],
+    "Salle de conférence": ["Manager", "Réceptionniste", "Technicien IT", "Agent de sécurité", "Nettoyage", "Autres"],
+    "Salle d'archives": ["Manager"]
+};
 
-function get_role_by_room_name(room_name,container_){
-    container_.innerHTML=""
-    let data = JSON.parse(localStorage.getItem("employer")) || []
-    let roles = {
-        "Reception": ["Manager", "Réceptionniste", "Nettoyage"],
-        "Salle des serveurs" : ["Manager", "Technicien IT", "Nettoyage"],
-        "Salle de sécurité":["Manager", "Agent de sécurité", "Nettoyage"],
-        "Salle du personnel": ["Manager", "Réceptionniste", "Technicien IT", "Agent de sécurité", "Nettoyage", "Autres"],
-        "Salle de conférence": ["Manager", "Réceptionniste", "Technicien IT", "Agent de sécurité", "Nettoyage", "Autres"],
-        "Salle d'archives": ["Manager"]
+    container_.innerHTML = "";
+    let employes = JSON.parse(localStorage.getItem("employer")) || [];
+    let rolesAcceptes = roles_zone[room_name] || [];
+    
+    for (let i = 0; i < employes.length; i++) {
+        let emp = employes[i];
+      if (emp.is_worked==false && rolesAcceptes.includes(emp.Role)) {
+            
+            container_.innerHTML += `
+        <div class="shadow-lg rounded-xl p-4 m-2 flex items-center gap-4 ">
+                <div data-id="${emp.id}">
+                    <img src="${emp.Url}" class="rounded-full w-20 h-20">
+                    <div class="flex flex-col">
+                        <h5 class="font-bold">${emp.id}</h5>
+                        <h2 class="font-bold text-lg">${emp.Nom}</h2>
+                        <p class="text-gray-600">${emp.Role}</p>
+                    </div>
+                </div>
+                </div>
+            `;
+        }
+    }
+
+   
+    container_.addEventListener("click",function (e){
+        let card = e.target.closest(".card");
+        if (!card) return;
+        let id = parseInt(card.getAttribute("data-id"));
+        let data = JSON.parse(localStorage.getItem("employer")) || [];
+
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].id === id) {
+                data[i].is_worked = true;
+                data[i].zone_work = container_.id;
+                window.location.reload();
+                break;
+                
+            }
+        }
+
+        localStorage.setItem("employer", JSON.stringify(data));
+        AfficherCarte(data);
+        afficherEmployesZone(room_name, container_);
+        alert("Employé affecté à " + room_name);
+    });
 }
 
-for(persons of Object.entries(data)){
+function  aficher_data(){
+    let employes = JSON.parse(localStorage.getItem("employer")) || [];
+for(_data_ of employes ){
 
-    for(room_ of Object.entries(roles)){
-       if(room_[0] == room_name){
+        if(_data_.is_worked){
 
-        for(let data_ of room_[1]){
-          if(data_==persons.Role){
-            
-        container_.innerHTML+=`
-                <div class="card shadow-lg rounded-xl p-4 m-2 flex items-center gap-4 "> 
-                <img src="${persons.Url}" class="rounded-full w-20 h-20">
-            
-                <div class=" flex flex-col">
-                <h5 class="id_card font-bold text-lg">${persons.id}</h5> 
-                <h2 class="name font-bold text-lg">${persons.Nom}</h2> 
-                <p class="text-gray-600">${persons.Role}</p>
-            </div>
-          </div>
-    
-     `;
-          }
-
-        }
-        
-          
-       }
-       
+   document.getElementById(_data_.zone_work).innerHTML+=
+              `<div class="card bg-red-50 rounded-xl p-4 m-2 flex items-center gap-4" data-id="${_data_.id}">
+                    <img src="${_data_.Url}" class="rounded-full w-20 h-20">
+                    <div class="flex flex-col">
+                        <h5 class="font-bold">${_data_.id}</h5>
+                        <h2 class="font-bold text-lg">${_data_.Nom}</h2>
+                        <p class="text-gray-600">${_data_.Role}</p>
+                    </div>
+                </div>
+             `;
     }
 }
-
-
 }
-
-
-function fetch_data(container_to_fetch){
-container_to_fetch.innerHTML=""
-let data_1 = JSON.parse(localStorage.getItem("employer")) || []
-for(data__ of Object.entries(data_1)){
-if(data__.id==true){
-    container_to_fetch.innerHTML+=`
-                <div class="card shadow-lg rounded-xl p-4 m-2 flex items-center gap-4 "> 
-                <img src="${data__.Url}" class="rounded-full w-20 h-20">
-                <div class=" flex flex-col">
-                <h5 class="id_card font-bold text-lg">${data__.id}</h5> 
-                <h2 class="name font-bold text-lg">${data__.Nom}</h2> 
-                <p class="text-gray-600">${data__.Role}</p>
-            </div>
-          </div>
-     `;
-}
-}
-}
-fetch_data(container_reseption_)
-
 
 _room1.addEventListener("click",()=>{
+afficherEmployesZone("Salle de conférence", document.getElementById("container_conference"))
 
-get_role_by_room_name("Salle de conférence")
 })
-
+aficher_data()
 _room2.addEventListener("click",()=>{
-
-get_role_by_room_name("Reception",container_reseption_)
-
-container_reseption_.addEventListener("click",function(e){
-let card = e.target.closest(".card")
-let id_clicked=card.querySelector("h5").textContent
-let data_2 = JSON.parse(localStorage.getItem("employer")) || []
-for(update_umploiyer of Object.entries(data_2)){
-    if(update_umploiyer.id ===id_clicked){
-        update_umploiyer.is_worked=true
-        update_umploiyer.zone_work="Reception"
-        localStorage.setItem("employer",JSON.stringify(data_2));
-    }
-}
-  
-})
-
-
-
-
+afficherEmployesZone("Reception",document.getElementById("container_reseption_"))
 
 })
 _room3.addEventListener("click",()=>{
-
-get_role_by_room_name("Salle des serveurs")
+afficherEmployesZone("Salle des serveurs",document.getElementById("container_serveurs"))
 })
+
 _room4.addEventListener("click",()=>{
-get_role_by_room_name("Salle de sécurité")
-
+    afficherEmployesZone("Salle de sécurité", document.getElementById("container_securite"));
 })
+
 _room5.addEventListener("click",()=>{
-
-get_role_by_room_name("Salle du personnel")
+afficherEmployesZone("Salle du personnel", document.getElementById("container_personnel"));
 })
+
+
 _room6.addEventListener("click",()=>{
-
-get_role_by_room_name("Salle d'archives")
+afficherEmployesZone("Salle d'archives", document.getElementById("container_archives"));
 })
 
 
+// _room2.onclick = () => afficherEmployesZone("Reception", document.getElementById("container_reseption_"));
+// _room3.onclick = () => afficherEmployesZone("Salle des serveurs", document.getElementById("container_serveurs"));
 
+// _room4.onclick = () => afficherEmployesZone("Salle de sécurité", document.getElementById("container_securite"));
+
+// _room5.onclick = () => afficherEmployesZone("Salle du personnel", document.getElementById("container_personnel"));
+
+// _room6.onclick = () => afficherEmployesZone("Salle d'archives", document.getElementById("container_archives"));
